6e3990b62cf6d858e4f79c052ab83cca80a295bb
Angelo Pirola <angelo9871@gmail.com>
2024-03-28 00:15:05 +0100

    Creato progetto Shared

---
+++ src/Microservice.Shared/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Microservice.Shared;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Sku { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public double Price { get; set; }
+    public string Brand { get; set; } = string.Empty;
+}
---
+++ src/Microservice.Shared/RabbitConfiguration.cs
@@ -0,0 +1,12 @@
+﻿namespace Microservice.Shared;
+public static class RabbitConfiguration
+{
+    public static string HostName => "rabbitmq://localhost";
+    public static string Username => "guest";
+    public static string Password => "guest";
+    public static string QueueName => "cart";
+    public static int PrefetchCount => 16;
+    public static int RetryCount => 3;
+    public static int RetryInterval => 100;
+    public static string EndpointCart => string.Concat(HostName, "/cart");
+}
