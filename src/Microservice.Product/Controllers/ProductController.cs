2bdc0931e8db854103bbe963ab11220851f20169
Angelo Pirola <angelo9871@gmail.com>
2024-03-28 00:14:59 +0100

    Creato progetto Prodotti

---
+++ src/Microservice.Product/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using MassTransit;
+using Microservice.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Microservice.Product.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductController(IBus bus) : ControllerBase
+{
+    private readonly IBus bus = bus;
+
+    [HttpPost]
+    public async Task<IActionResult> CreateCartAsync(Shared.Product product)
+    {
+        if (product != null)
+        {
+            var uri = new Uri(RabbitConfiguration.EndpointCart);
+            var endPoint = await bus.GetSendEndpoint(uri);
+            await endPoint.Send(product);
+            return Ok();
+        }
+
+        return BadRequest();
+    }
+}
---
+++ src/Microservice.Product/Program.cs
@@ -0,0 +1,31 @@
+﻿using MassTransit;
+using Microservice.Shared;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddMassTransit(x =>
+{
+    x.UsingRabbitMq((context, config) =>
+    {
+        config.Host(new Uri(RabbitConfiguration.HostName), h =>
+        {
+            h.Username(RabbitConfiguration.Username);
+            h.Password(RabbitConfiguration.Password);
+        });
+    });
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+app.Run();
